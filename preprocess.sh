1cf2e79c8817f3c7459b3dce92c73ba4b10ce9b9
epicrunze <epicrunze@gmail.com>
2021-05-31 23:16:28 -0400

    tweaked run scripts

--- preprocess.sh
+++ preprocess.sh
@@ -6,13 +6,12 @@
 ### GRAHAM: v100, t4
 ### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
 
-#SBATCH --gres=gpu:v100l:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=32000
 #SBATCH --account=def-plato
-#SBATCH --time=24:0:0
+#SBATCH --time=8:0:0
 
 source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
-python src/adas/train.py --config src/adas/configSGD.yaml --output .SGD-output/ADP --checkpoint .SGD-checkpoint/ADP
+python src/adas/preprocess_datasets.py
