ad12fc302b477a30d65a3f41de8b1ff8733af9df
Ryan Zhang <zhan8425@cedar1.cedar.computecanada.ca>
2021-06-04 08:29:22 -0700

    compute canada update

--- runslurm.sh
+++ runslurm.sh
@@ -6,12 +6,13 @@
 ### GRAHAM: v100, t4
 ### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
 
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=32000M
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=32000
 #SBATCH --account=def-plato
-#SBATCH --time=18:0:0
+#SBATCH --time=0:10:0
 
 source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
-### python src/adas/process_datasets.py
-python src/adas/get_norm.py
+python src/adas/train.py --config src/adas/configSGD.yaml --output .SGD-output/ADP --checkpoint .SGD-checkpoint/ADP
