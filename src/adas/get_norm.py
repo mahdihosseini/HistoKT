12fdc869d7e7b5beb9e2abdbd4df0316194c523e
epicrunze <epicrunze@gmail.com>
2021-06-04 11:33:32 -0400

    added prints and timing

--- src/adas/get_norm.py
+++ src/adas/get_norm.py
@@ -2,6 +2,7 @@ from utils import get_mean_and_std
 from datasets import TransformedDataset
 from torchvision.transforms import ToTensor
 import os
+import time
 
 
 def main(root):
@@ -14,11 +15,16 @@ def main(root):
                     "MHIST_transformed",
                     "OSDataset_transformed",
                     "PCam_transformed"]:
+
+        print(f"Loading dataset: {dataset}")
+        start_time = time.time()
         dataset_object = TransformedDataset(transform=ToTensor(),
                                             root=os.path.join(root, dataset),
                                             split='train')
-
+        load_time = time.time()
+        print(f"Loaded dataset, took time:", load_time-start_time)
         mean, std = get_mean_and_std(dataset_object, num_workers=4)
+        print("calculated mean and std, took time:", time.time()-load_time)
         output_dict[dataset] = {"mean": mean, "std": std}
 
     return output_dict
