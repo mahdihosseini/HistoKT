6c882da2d8626f3764b549a22da74c9b44c348af
epicrunze <epicrunze@gmail.com>
2021-06-08 11:59:54 -0400

    ADP training config change

--- runADP.sh
+++ runADP.sh
@@ -6,13 +6,13 @@
 ### GRAHAM: v100, t4
 ### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
 
-#####SBATCH --gres=gpu:v100l:1
+#SBATCH --gres=gpu:v100l:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
 #SBATCH --mem=32000M
 #SBATCH --account=def-plato
-#SBATCH --time=00:30:00
+#SBATCH --time=23:00:00
 #SBATCH --output=%x-%j.out
 
 # prepare data
@@ -24,7 +24,5 @@ tar xf /home/zhan8425/scratch/HistoKTdata/ADP\ V1.0\ Release.tar -C $SLURM_TMPDI
 echo "Finished transferring"
 echo ""
 
-ls -a $SLURM_TMPDIR
-
-#source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
-#python src/adas/train.py --config src/adas/HistoKTconfigs/ADP-configAdas.yaml --output .Adas-output/ADP --checkpoint .Adas-checkpoint/ADP --data $SLURM_TMPDIR
+source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
+python src/adas/train.py --config src/adas/HistoKTconfigs/ADP-configAdas.yaml --output .Adas-output/ADP --checkpoint .Adas-checkpoint/ADP --data $SLURM_TMPDIR
