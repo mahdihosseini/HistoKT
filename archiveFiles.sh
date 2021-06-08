cd3736827a79be0aa4f1721bd38eee66420f0e35
epicrunze <epicrunze@gmail.com>
2021-06-08 12:51:55 -0400

    testing archive script

---
+++ archiveFiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ROOT_PATH="/home/zhan8425/scratch/HistoKTdata"
+
+for DATASET in AIDPATH_transformed AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed
+do
+  echo $ROOT_PATH/$DATASET.tar
+  echo $ROOT_PATH/$DATASET/*
+  #tar cf $ROOT_PATH/$DATASET.tar $ROOT_PATH/$DATASET/*
+done
