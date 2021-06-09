b171a30db41a9ec041e443f2c4a08931aa97c7c2
epicrunze <epicrunze@gmail.com>
2021-06-09 10:02:26 -0400

    updated archive paths

--- runGlaS_transformed.sh
+++ runGlaS_transformed.sh
@@ -19,7 +19,6 @@
 
 echo "transferring data"
 echo ""
-mkdir $SLURM_TMPDIR/data
 tar xf /home/zhan8425/scratch/HistoKTdata/GlaS_transformed.tar -C $SLURM_TMPDIR
 echo "Finished transferring"
 echo ""
