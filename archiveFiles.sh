fc4d708ff392c9ce98471bc6fe160fc0fdda5723
epicrunze <epicrunze@gmail.com>
2021-06-09 10:47:29 -0400

    updated archive paths

--- archiveFiles.sh
+++ archiveFiles.sh
@@ -2,7 +2,7 @@
 
 ROOT_PATH="/home/zhan8425/scratch/HistoKTdata"
 
-for DATASET in AIDPATH_transformed #AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed
+for DATASET in AIDPATH_transformed AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed
 do
   echo $ROOT_PATH/$DATASET.tar
   tar cf $ROOT_PATH/$DATASET.tar -C $ROOT_PATH $DATASET
