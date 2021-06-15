b2d6314a07bbbdbc5eb16dcbe1eb26faa062aed1
epicrunze <epicrunze@gmail.com>
2021-06-15 00:13:48 -0400

    tweaked code

--- dataset_processing/create_archives.py
+++ dataset_processing/create_archives.py
@@ -5,16 +5,23 @@ from datasets import TransformedDataset
 
 def main(root):
     for dataset_name in ["AIDPATH_transformed"]:
+        num_per_class_list = [100, 200, 300, 500, 1000]
+        archive_names = [os.path.join(root, dataset_name + f"_{num_per_class}_per_class")
+                         for num_per_class in num_per_class_list]
         split = "train"
-        dataset = TransformedDataset(os.path.join(root, dataset_name), split=split)
-        for num in [100, 200, 300, 500, 1000]:
-            archive_subdataset(dataset, os.path.join(root, dataset_name+f"_{num}_per_class"), num_per_class=num)
+        archive_subdataset(os.path.join(root, dataset_name),
+                           split,
+                           archive_names=archive_names,
+                           num_per_class_list=num_per_class_list)
         split = "valid"
-        dataset = TransformedDataset(os.path.join(root, dataset_name), split=split)
-        for num in [100, 200, 300, 500, 1000]:
-            archive_subdataset(dataset, os.path.join(root, dataset_name + f"_{num}_per_class"), num_per_class=200)
+        valid_num_per_class_list = [200 for _ in range(len(num_per_class_list))]
+        archive_subdataset(os.path.join(root, dataset_name),
+                           split,
+                           archive_names=archive_names,
+                           num_per_class_list=valid_num_per_class_list)
 
 
 if __name__ == "__main__":
-    root_dir = "/home/zhan8425/scratch/HistoKTdata"
+    # root_dir = "/home/zhan8425/scratch/HistoKTdata"
+    root_dir = "C:\\Users\\ryanr\\Desktop\\Summer_Research\\HistoKT\\.adas-data"
     main(root_dir)
--- dataset_processing/utils.py
+++ dataset_processing/utils.py
@@ -55,43 +55,44 @@ def get_mean_and_std(dataset: torch.utils.data.dataset,
     return mean, std
 
 
-def archive_subdataset(dataset: torch.utils.data.dataset,
-                       tar_name: str,
-                       split_by_classes: bool = True,
-                       num_per_class: int = 100):
+def archive_subdataset(root,
+                       split,
+                       archive_names: list = [],
+                       num_per_class_list: list = []):
     # creating dictionary with class index as a key, and [filenames, ] as the value
 
     class_to_files = defaultdict(list)
-    samples = pickle.load(open(os.path.join(dataset.root, dataset.split + ".pickle"), "rb"))
+    samples = pickle.load(open(os.path.join(root, split + ".pickle"), "rb"))
     random.seed(42)
     random.shuffle(samples)
 
     for filename, label in samples:
         class_to_files[label].append(filename)
 
-    try:
-        new_samples = [(file_list[i], label)
-                       for label, file_list in class_to_files.items()
-                       for i in range(num_per_class)]
-    except IndexError as err:
-        print(f"not {num_per_class} enough of a class: {[(key, len(value)) for key, value in class_to_files.items()]}")
-        raise err
-    # getting list of files to tar
-    files_to_tar = [path for path, label in new_samples]
+    for num_per_class, tar_name in zip(num_per_class_list, archive_names):
+        try:
+            new_samples = [(file_list[i], label)
+                           for label, file_list in class_to_files.items()
+                           for i in range(num_per_class)]
+        except IndexError as err:
+            class_list = [(key, len(value)) for key, value in class_to_files.items()]
+            print(f"not {num_per_class} enough of a class: {class_list}")
+            raise err
+        # getting list of files to tar
+        files_to_tar = [path for path, label in new_samples]
 
-    # tarring files
-    with tarfile.open(os.path.join(os.path.dirname(dataset.root), tar_name+".tar"), "a") as tar:
-        archive_name = os.path.basename(dataset.root)
-        for fn in files_to_tar:
-            tar.add(os.path.join(dataset.root, fn), arcname=os.path.join(archive_name, fn))
+        # tarring files
+        with tarfile.open(os.path.join(os.path.dirname(root), tar_name+".tar"), "a") as tar:
+            archive_name = os.path.basename(root)
+            for fn in files_to_tar:
+                tar.add(os.path.join(root, fn), arcname=os.path.join(archive_name, fn))
 
-        # adding pickles
-        with open(os.path.join(tempfile.gettempdir(), f"{dataset.split}.pickle"), "wb") as file:
-            pickle.dump(new_samples, file)
-        tar.add(os.path.join(tempfile.gettempdir(), f"{dataset.split}.pickle"),
-                arcname=os.path.join(archive_name, f"{dataset.split}.pickle"))
+            # adding pickles
+            with open(os.path.join(tempfile.gettempdir(), f"{split}.pickle"), "wb") as file:
+                pickle.dump(new_samples, file)
+            tar.add(os.path.join(tempfile.gettempdir(), f"{split}.pickle"),
+                    arcname=os.path.join(archive_name, f"{split}.pickle"))
 
-        # adding class_to_idx pickle
-        tar.add(os.path.join(dataset.root, "class_to_idx.pickle"),
-                arcname=os.path.join(archive_name, "class_to_idx.pickle"))
-
+            # adding class_to_idx pickle
+            tar.add(os.path.join(root, "class_to_idx.pickle"),
+                    arcname=os.path.join(archive_name, "class_to_idx.pickle"))
