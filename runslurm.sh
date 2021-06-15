aa94fbd316ea405bee6e56e560b5b11a5cd003c6
epicrunze <epicrunze@gmail.com>
2021-06-15 00:39:55 -0400

    created script to run stuff

--- runslurm.sh
+++ runslurm.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-### GPU OPTIONS:
-### CEDAR: v100l, p100
-### BELUGA: *no option, just use --gres=gpu:*COUNT*
-### GRAHAM: v100, t4
-### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
-
-#SBATCH --gres=gpu:v100l:1
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=32000
-#SBATCH --account=def-plato
-#SBATCH --time=0:10:0
-
-source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
-python src/adas/train.py --config src/adas/configSGD.yaml --output .SGD-output/ADP --checkpoint .SGD-checkpoint/ADP
+for DATASET in CRC_transformed PCam_transformed
+do
+  for NUM in 100 200 300 500 1000
+  do
+    echo "sbatch run${DATASET}_transformed_${NUM}_per_class.sh"
+    sleep 2
+  done
+done
