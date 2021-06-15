95ed450786672bb355956506d582d537d6bc1a65
epicrunze <epicrunze@gmail.com>
2021-06-14 23:23:59 -0400

    tweaked code

--- dataset_processing/create_archives.py
+++ dataset_processing/create_archives.py
@@ -6,12 +6,12 @@ from datasets import TransformedDataset
 def main(root):
     for dataset_name in ["AIDPATH_transformed"]:
         split = "train"
+        dataset = TransformedDataset(os.path.join(root, dataset_name), split=split)
         for num in [100, 200, 300, 500, 1000]:
-            dataset = TransformedDataset(os.path.join(root, dataset_name), split=split)
             archive_subdataset(dataset, os.path.join(root, dataset_name+f"_{num}_per_class"), num_per_class=num)
         split = "valid"
+        dataset = TransformedDataset(os.path.join(root, dataset_name), split=split)
         for num in [100, 200, 300, 500, 1000]:
-            dataset = TransformedDataset(os.path.join(root, dataset_name), split=split)
             archive_subdataset(dataset, os.path.join(root, dataset_name + f"_{num}_per_class"), num_per_class=200)
 
 
