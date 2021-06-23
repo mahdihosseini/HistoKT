58c953d17f15cc38789c05e5f8109022cc722627
epicrunze <epicrunze@gmail.com>
2021-06-23 10:37:12 -0400

    cleaned up files

---
+++ old_run_scripts/June 23rd/runGlaS_transformed.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+### GPU OPTIONS:
+### CEDAR: v100l, p100
+### BELUGA: *no option, just use --gres=gpu:*COUNT*
+### GRAHAM: v100, t4
+### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
+
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=32000M
+#SBATCH --account=def-plato
+#SBATCH --time=8:00:00
+#SBATCH --output=%x-%j.out
+
+# prepare data
+
+echo "transferring data"
+echo ""
+tar xf /home/zhan8425/scratch/HistoKTdata/GlaS_transformed.tar -C $SLURM_TMPDIR
+echo "Finished transferring"
+echo ""
+
+source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
+python src/adas/train.py --config HistoKTconfigs/GlaS_transformed-configAdas.yaml --output .Adas-output/GlaS_transformed --checkpoint .Adas-checkpoint/GlaS_transformed --data $SLURM_TMPDIR
