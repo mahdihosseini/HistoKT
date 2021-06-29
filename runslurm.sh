0dde2d4b8e2e2468a59ab4858ada8ffeed1a1286
epicrunze <epicrunze@gmail.com>
2021-06-29 15:15:09 -0400

    added more SAM trials

--- runslurm.sh
+++ runslurm.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 
-for NUM in 0.001 0.0005 0.0003 0.0001 0.00005
-do
-  sbatch runMHIST_transformed-AdaM-lr-${NUM}.sh
-  sleep 2
-done
-
-for NUM in 0.5 0.3 0.1 0.05 0.03
-do
-  sbatch runMHIST_transformed-SGDP-lr-${NUM}.sh
-  sleep 2
-done
-
-for NUM in 3.0 1.0 0.5 0.3 0.1
+for NUM in 0.05 0.03 0.01 0.005
 do
   sbatch runMHIST_transformed-SAM-lr-${NUM}.sh
   sleep 2
