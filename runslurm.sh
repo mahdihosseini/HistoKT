44f70e99cfb9a35da915ddfb3938765fb2e75d25
epicrunze <epicrunze@gmail.com>
2021-06-29 18:29:23 -0400

    added more test scripts for Adas, SAM, SGDP

--- runslurm.sh
+++ runslurm.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-for NUM in 0.05 0.03 0.01 0.005
+for NUM in 0.00003 0.00001 0.0000005
 do
-  sbatch runMHIST_transformed-SAM-lr-${NUM}.sh
+  sbatch runMHIST_transformed-AdaM-lr-${NUM}.sh
+  sleep 2
+done
+
+for NUM in 0.01 0.005 0.003
+do
+  sbatch runMHIST_transformed-SGDP-lr-${NUM}.sh
+  sleep 2
+done
+
+for NUM in 0.07 0.1 0.3
+do
+  sbatch runMHIST_transformed-Adas-lr-${NUM}.sh
   sleep 2
 done
