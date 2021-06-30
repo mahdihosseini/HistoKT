ce2db829dd55a9aeb5c00a70bca21a740b9e329f
epicrunze <epicrunze@gmail.com>
2021-06-30 12:18:47 -0400

    added more test scripts for AdaM

--- runslurm.sh
+++ runslurm.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-for NUM in 0.00003 0.00001 0.0000005
+for NUM in 0.00001 0.000005
 do
   sbatch runMHIST_transformed-AdaM-lr-${NUM}.sh
-  sleep 2
-done
-
-for NUM in 0.01 0.005 0.003
-do
-  sbatch runMHIST_transformed-SGDP-lr-${NUM}.sh
-  sleep 2
-done
-
-for NUM in 0.07 0.1 0.3
-do
-  sbatch runMHIST_transformed-Adas-lr-${NUM}.sh
   sleep 2
 done
