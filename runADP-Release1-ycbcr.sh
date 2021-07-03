24c89e822983aa60d971899b48a43d4d0c78a2c6
epicrunze <epicrunze@gmail.com>
2021-07-03 00:53:49 -0400

    added ycbcr scripts

---
+++ runADP-Release1-ycbcr.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+### GPU OPTIONS:
+### CEDAR: v100l, p100
+### BELUGA: *no option, just use --gres=gpu:*COUNT*
+### GRAHAM: v100, t4
+### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
+
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000M
+#SBATCH --account=def-plato
+#SBATCH --time=12:00:00
+#SBATCH --output=%x-%j.out
+
+# prepare data
+
+echo "transferring data"
+echo ""
+date
+mkdir $SLURM_TMPDIR/data
+tar xf /home/zhan8425/scratch/HistoKTdata/ADP-Release1.tar -C $SLURM_TMPDIR
+echo "Finished transferring"
+echo ""
+date
+
+source ~/projects/def-plato/zhan8425/HistoKT/ENV/bin/activate
+python src/adas/train.py --config ~/projects/def-plato/zhan8425/HistoKT/PretrainingConfigs/ADP-Release1-ycbcr-configAdas.yaml --output pretraining-output/ycbcr/ADP-Release1 --checkpoint pretraining-checkpoint/ycbcr/ADP-Release1 --data $SLURM_TMPDIR
--- runslurm.sh
+++ runslurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-for NUM in 0.00001 0.000005
+for DATASET in ADP-Release1 AIDPATH_transformed AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed
 do
-  sbatch runMHIST_transformed-AdaM-lr-${NUM}.sh
+  sbatch run${DATASET}-ycbcr.sh
   sleep 2
 done
