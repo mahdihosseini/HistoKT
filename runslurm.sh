cad3479f7d311cdc452f0d36507641e46e6f09bb
epicrunze <epicrunze@gmail.com>
2021-07-05 14:57:43 -0400

    rerunning pretrained weights

--- runslurm.sh
+++ runslurm.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-for DATASET in AIDPATH_transformed AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed
+for DATASET in ADP-Release1 AIDPATH_transformed AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed
 do
-  for LR in 0.001 0.0005 0.0002 0.0001 0.00005
-  do
-    sbatch run${DATASET}-AdamP-lr-${LR}.sh
-    sleep 2
-  done
+  sbatch run${DATASET}-Colour-Distortion.sh
+  sleep 2
 done
