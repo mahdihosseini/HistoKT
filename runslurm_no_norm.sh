8e747f985fb03e35f5683eed9a147a0c19806ce8
Ryan Zhang <runze.zhang2001@gmail.com>
2021-07-11 11:07:58 -0400

    updated

--- runslurm_no_norm.sh
+++ runslurm_no_norm.sh
@@ -4,7 +4,7 @@ for DATASET in PCam_transformed
 do
   for LR in 0.001 0.0005 0.0002 0.0001 0.00005
   do
-    sbatch run${DATASET}-AdamP-lr-${LR}-ADP-norm-ADP-Release1.sh
+    sbatch run${DATASET}-AdamP-lr-${LR}-ADP-norm-no_norm.sh
     sleep 2
   done
 done
