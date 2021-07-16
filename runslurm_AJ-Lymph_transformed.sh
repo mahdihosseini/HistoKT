ee7f803e4dfa185f6d36d9d6d97c6fcbf14939d2
Ryan Zhang <zhan8425@beluga3.int.ets1.calculquebec.ca>
2021-07-16 14:23:23 -0400

    other deep-tuning

--- runslurm_AJ-Lymph_transformed.sh
+++ runslurm_AJ-Lymph_transformed.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-BCSS_transformed-norm-BCSS_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-ADP-Release1-norm-ADP-Release1-None.sh
 sleep 2
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-OSDataset_transformed-norm-OSDataset_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-BCSS_transformed-norm-BCSS_transformed-None.sh
 sleep 2
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-CRC_transformed-norm-CRC_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-OSDataset_transformed-norm-OSDataset_transformed-None.sh
 sleep 2
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-BACH_transformed-norm-BACH_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-CRC_transformed-norm-CRC_transformed-None.sh
 sleep 2
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-GlaS_transformed-norm-GlaS_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-BACH_transformed-norm-BACH_transformed-None.sh
 sleep 2
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-MHIST_transformed-norm-MHIST_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-GlaS_transformed-norm-GlaS_transformed-None.sh
 sleep 2
-sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-PCam_transformed-norm-PCam_transformed-Color-Distortion-0.1.sh
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-MHIST_transformed-norm-MHIST_transformed-None.sh
+sleep 2
+sbatch runAJ-Lymph_transformed-AdamP-lr-0.0002-PCam_transformed-norm-PCam_transformed-None.sh
 sleep 2
--- runslurm_BACH_transformed.sh
+++ runslurm_BACH_transformed.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-sbatch runBACH_transformed-AdamP-lr-0.00005-BCSS_transformed-norm-BCSS_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-ADP-Release1-norm-ADP-Release1-None.sh
 sleep 2
-sbatch runBACH_transformed-AdamP-lr-0.00005-OSDataset_transformed-norm-OSDataset_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-BCSS_transformed-norm-BCSS_transformed-None.sh
 sleep 2
-sbatch runBACH_transformed-AdamP-lr-0.00005-CRC_transformed-norm-CRC_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-OSDataset_transformed-norm-OSDataset_transformed-None.sh
 sleep 2
-sbatch runBACH_transformed-AdamP-lr-0.00005-AJ-Lymph_transformed-norm-AJ-Lymph_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-CRC_transformed-norm-CRC_transformed-None.sh
 sleep 2
-sbatch runBACH_transformed-AdamP-lr-0.00005-GlaS_transformed-norm-GlaS_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-AJ-Lymph_transformed-norm-AJ-Lymph_transformed-None.sh
 sleep 2
-sbatch runBACH_transformed-AdamP-lr-0.00005-MHIST_transformed-norm-MHIST_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-GlaS_transformed-norm-GlaS_transformed-None.sh
 sleep 2
-sbatch runBACH_transformed-AdamP-lr-0.00005-PCam_transformed-norm-PCam_transformed-Color-Distortion-0.1.sh
+sbatch runBACH_transformed-AdamP-lr-0.00005-MHIST_transformed-norm-MHIST_transformed-None.sh
+sleep 2
+sbatch runBACH_transformed-AdamP-lr-0.00005-PCam_transformed-norm-PCam_transformed-None.sh
 sleep 2
