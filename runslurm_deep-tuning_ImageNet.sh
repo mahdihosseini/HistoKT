15a9680d83ab440866edb5b20a73a533d2ca8f21
epicrunze <epicrunze@gmail.com>
2021-07-17 14:00:37 -0400

    Imagenet deep-tuning, plato account

--- runslurm_deep-tuning_ImageNet.sh
+++ runslurm_deep-tuning_ImageNet.sh
@@ -2,5 +2,5 @@
 
 for DATASET in ADP-Release1 AJ-Lymph_transformed BACH_transformed CRC_transformed GlaS_transformed MHIST_transformed OSDataset_transformed PCam_transformed BCSS_transformed
 do
-  sh runslurm${DATASET}.sh
+  sh runslurm_${DATASET}.sh
 done
