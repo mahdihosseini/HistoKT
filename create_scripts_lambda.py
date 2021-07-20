3e18fe74f333dab346e4943821f14e6790ade97f
epicrunze <epicrunze@gmail.com>
2021-07-20 17:12:32 -0400

    new weight transfer scripts

--- create_scripts_lambda.py
+++ create_scripts_lambda.py
@@ -1,5 +1,5 @@
 import os
-
+from collections import defaultdict
 
 best_lrs = {
     "ADP-Release1": 0.0001,
@@ -465,7 +465,246 @@ date
             outfile.write("".join(outlines))
 
 
+def run_fine_tune_combined(root, CC=False, node="cedar", account="def-msh"):
+    runscripts = defaultdict(list)
+    # account = "def-plato"
+    # account = "def-msh"
+
+    if node == "cedar":
+        gpu = "v100l"
+    elif node == "beluga":
+        gpu = "v100"
+    elif node == "graham":
+        gpu = "p100"
+
+    optimizer = "AdamP"
+
+    freeze_encoders = [
+        # "True",
+        "False"
+    ]
+
+    color_aug = "None"
+
+    datasets = [
+        # "ADP-Release1",
+        # "BCSS_transformed",
+        # "OSDataset_transformed",
+        # "CRC_transformed",
+        # "AJ-Lymph_transformed",
+        "BACH_transformed",
+        # "GlaS_transformed",
+        "MHIST_transformed",
+        # "PCam_transformed",
+    ]
+    gpu_start = 0
+
+    weight_folders = [
+        "None_1",
+        "None_2",
+        "ImageNet_1",
+        "ImageNet_2"
+    ]
+
+    learning_rates = ["0.001", "0.0005", "0.0002", "0.0001", "0.00005"]
+
+    for weight_folder in weight_folders:
+        for dataset in datasets:
+            # choose from no_norm, target_domain, or a dataset name
+            if CC:
+                if node == "cedar":
+                    data_storage = "/home/zhan8425/scratch/HistoKTdata"
+                    env_root = "/home/zhan8425/projects/def-plato/zhan8425/HistoKT"
+                elif node == "beluga":
+                    data_storage = "/scratch/stephy/HistoKTdata"
+                    env_root = "/home/zhan8425/projects/def-msh/zhan8425/HistoKT"
+                elif node == "graham":
+                    data_storage = "/scratch/stephy/HistoKTdata"
+                    env_root = "/home/zhan8425/projects/def-msh/zhan8425/HistoKT"
+                env_name = "ENV"
+                data_dir = "$SLURM_TMPDIR"
+            else:
+                data_storage = "None"
+                env_root = "/ssd2/HistoKT/source"
+                env_name = "env"
+                data_dir = f"/ssd{gpu_start + 1}/users/mhosseini/datasets/"
+
+            pretrained_models = os.listdir(os.path.join(root, weight_folder))
+            for pretrained_model in pretrained_models:
+                normalization_all = "target_domain" if "ImageNet" not in weight_folder else "ImageNet"
+                pretrained_model = os.path.join(root, weight_folder, pretrained_model)
+                pretrained_model_name = pretrained_model.split(".")[0]
+                for learning_rate in learning_rates:
+                    os.makedirs(f"WeightSharingTrainingConfigs/{dataset}/{optimizer}", exist_ok=True)
+                    with open(os.path.join(root, f"WeightSharingTrainingConfigs/{dataset}/{optimizer}/lr-{learning_rate}-config.yaml"),
+                              "w") as write_file:
+                        if "ADP" in dataset or "BCSS_transformed" in dataset:
+                            loss_fn = "MultiLabelSoftMarginLoss"
+                        else:
+                            loss_fn = "cross_entropy"
+                        data = f"""###### Application Specific ######
+dataset: '{dataset}' # options: CIFAR100, CIFAR10, ImageNet, ADP-Release1, MHIST
+# To use these datasets, please transform them using standardize_datasets.py
+# AIDPATH_transformed
+# AJ-Lymph_transformed
+# BACH_transformed
+# CRC_transformed
+# GlaS_transformed
+# MHIST_transformed
+# OSDataset_transformed
+# PCam_transformed
+# BCSS_transformed
+network: 'ResNet18' # AlexNet, DenseNet201, DenseNet169, DenseNet161, DenseNet121, GoogLeNet
+# InceptionV3, MNASNet_0_5, MNASNet_0_75, MNASNet_1, MNASNet_1_3, MobileNetV2, ResNet18
+# ResNet34, ResNet50, ResNet101, ResNet152, ResNext50, ResNext101, WideResNet50, WideResNet101
+# ShuffleNetV2_0_5, ShuffleNetV2_1, ShuffleNetV2_1_5, ShuffleNetV2_2, SqueezeNet_1,
+# SqueezeNet_1_1, VGG11, VGG11_BN, VGG13, VGG13_BN, VGG16, VGG16_BN, VGG19, VGG19_BN,
+# EfficientNetB4
+# ResNet101CIFAR, ResNet50CIFAR, ResNet34CIFAR, ResNet18CIFAR, ResNeXtCIFAR, EfficientNetB0CIFAR, VGG16CIFAR, DenseNet121CIFAR
+optimizer: '{optimizer}' # options: SGD, AdaM, AdaGrad, RMSProp, AdaDelta
+scheduler: 'StepLR' # options: AdaS (with SGD), StepLR, CosineAnnealingWarmRestarts, OneCycleLR
+# ADP level
+level : 'L3' #L1, L2, L3, L3Only
+
+###### Augmentation Methods ######
+# ADP ONLY
+color_kwargs:
+    augmentation: '{color_aug}' # options: YCbCr, HSV, RGB-Jitter,
+                               # Color-Distortion (Color-Jittering followed by color drop),
+                               # None
+    distortion: 0.0 #options: 0.3 for Color-Distortion
+                            # 0.1 for YCbCr-Light and HSV-Light
+                            # 1.0 for YCbCr-Strong and HSV-Strong
+                            # 1.0 for RBGJitter
+degree_of_rotation: 45  # angle for random image rotation for torchvision.transforms.RandomAffine
+vertical_flipping: 0.5  #the probability of an image being vertically flipped
+horizontal_flipping: 0.5 #the probability of an image being horizontally flipped
+horizontal_shift: 0.1 #HorizontalTranslation
+vertical_shift: 0.1 #VerticalTranslation
+
+# ALL Datasets
+gaussian_blur: False # controlling the use of Gblur
+kernel_size: 9 #kernel size for the Gaussian Blur filter
+variance: 0.1 #variance of the Gaussian Blur Filter
+cutout: False # might change
+n_holes: 1
+cutout_length: 16
+
+###### Suggested Tune ######
+init_lr: {learning_rate}
+early_stop_threshold: -1 # set to -1 if you wish not to use early stop, or equally, set to a high value. Set to -1 if not using AdaS
+optimizer_kwargs:
+    weight_decay: 5e-4
+
+scheduler_kwargs: 
+    gamma: 0.5
+    step_size: 20
+
+###### Suggested Default ######
+p: 1 # options: 1, 2.
+start_trial: 0 # trial to start at, indexing from zero, default 0
+n_trials: 3 #increase to more to see more results
+num_workers: 4
+max_epochs: 250
+mini_batch_size: 64
+loss: '{loss_fn}' # options: cross_entropy, MultiLabelSoftMarginLoss
+early_stop_patience: 10 # epoch window to consider when deciding whether to stop"""
+                        write_file.write(data)
+
+                    runscripts[weight_folder].append(
+                        f"run{dataset}-{optimizer}-lr-{learning_rate}-{pretrained_model_name}-norm-{normalization_all}-{color_aug}.sh")
+
+                    with open(
+                            f"run{dataset}-{optimizer}-lr-{learning_rate}-{pretrained_model_name}-norm-{normalization_all}-{color_aug}.sh",
+                            "w") as outfile:
+                        time_taken = "11:00:00"
+                        if "CRC" in dataset:
+                            datafile = "CRC_transformed_2000_per_class"
+                            time_taken = "23:00:00"
+                        elif "PCam" in dataset:
+                            datafile = "PCam_transformed_1000_per_class"
+                        elif "ADP" in dataset:
+                            datafile = "ADP\\ V1.0\\ Release"
+                            time_taken = "23:00:00"
+                        elif "OSDataset_transformed" in dataset:
+                            time_taken = "23:00:00"
+                            datafile = dataset
+                        elif "BCSS_transformed" in dataset:
+                            datafile = dataset
+                            time_taken = "23:00:00"
+                        else:
+                            datafile = dataset
+
+                        if normalization_all == "target_domain":
+                            normalization = dataset
+                        else:
+                            normalization = normalization_all
+
+                        dataCC = f"""#!/bin/bash
+        
+### GPU OPTIONS:
+### CEDAR: v100l, p100
+### BELUGA: *no option, just use --gres=gpu:*COUNT*
+### GRAHAM: v100, t4
+### see https://docs.computecanada.ca/wiki/Using_GPUs_with_Slurm
+
+#SBATCH --gres=gpu:{gpu}:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=16000M
+#SBATCH --account={account}
+#SBATCH --time={time_taken}
+#SBATCH --output=%x-%j.out
+
+# prepare data
+
+echo "transferring data"
+echo ""
+date
+tar xf {data_storage}/{datafile}.tar -C $SLURM_TMPDIR
+echo "Finished transferring"
+echo ""
+date
+
+"""
+
+                        data = f"""source {env_root}/{env_name}/bin/activate\n"""
+                        for freeze_encoder in freeze_encoders:
+                            run_part = f"""python src/adas/train.py \
+--config {env_root}/WeightSharingTrainingConfigs/{dataset}/{optimizer}/lr-{learning_rate}-config.yaml \
+--output {pretrained_model_name}_norm_{normalization_all}_color_aug_None/{dataset}/{optimizer}/output/{"fine_tuning" if freeze_encoder == "True" else "deep_tuning"}/ \
+--checkpoint {pretrained_model_name}_norm_{normalization_all}_color_aug_None/{dataset}/{optimizer}/checkpoint/{"fine_tuning" if freeze_encoder == "True" else "deep_tuning"}/lr-{learning_rate} \
+--data {data_dir} \
+--pretrained_model {pretrained_model} \
+--freeze_encoder {freeze_encoder} \
+--save-freq 200 \
+--norm_vals {normalization} \
+{"" if CC else f"--gpu {gpu_start}"}
+        
+"""
+                            data += run_part
+
+                        outfile.write(data if not CC else dataCC + data)
+        gpu_start += 1
+
+    if CC:
+        for dataset in datasets:
+            with open(f"runslurm_weight_transfer_{weight_folder}.sh", "w") as outfile:
+                outlines = runscripts[weight_folder]
+                outfile.write("#!/bin/bash\n")
+                outfile.write("".join(outlines))
+    else:
+        for weight_folder in weight_folders:
+            with open(f"runlambda_weight_transfer_{weight_folder}.sh", "a") as outfile:
+                outlines = runscripts[weight_folder]
+
+                outfile.write("#!/bin/bash\n")
+                outfile.write("".join(outlines))
+
+
 if __name__ == "__main__":
     root_dir = ""
     # run_baselines(root_dir, CC=False, node="beluga")
-    run_fine_tune(root_dir, CC=False, node="graham", account="def-msh")
+    # run_fine_tune(root_dir, CC=False, node="graham", account="def-msh")
+    run_fine_tune_combined(root_dir, CC=False)
