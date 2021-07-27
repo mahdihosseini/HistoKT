49bbad3a742653bca3c0b9f98ddb8d90c60dd2f3
Stephen Yang <stephy.yang@mail.utoronto.ca>
2021-07-27 13:44:18 -0400

    updated bach gradcam run script

--- gradCAM/bach2.sh
+++ gradCAM/bach2.sh
@@ -44,7 +44,7 @@ python3 main.py --image_path=$ROOT/examples/BACH/ \
 python3 main.py --image_path=$ROOT/examples/BACH/ \
 --model_path=$WEIGHT_SHARING/None/ADP-Release1_CRC_combined_norm_target_domain_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0005/best_trial_0_date_2021-07-20-18-57-20.pth.tar \
 --dataset_name=BACH_transformed \
---output_path=output/BACH_ws_ADP+CRC_nocoloraug_imagenet \
+--output_path=output/BACH_ws_ADP+CRC_nocoloraug_none \
 --aug_smooth=True --eigen_smooth=True
 
 python3 main.py --image_path=$ROOT/examples/BACH/ \
@@ -56,17 +56,67 @@ python3 main.py --image_path=$ROOT/examples/BACH/ \
 python3 main.py --image_path=$ROOT/examples/BACH/ \
 --model_path=$WEIGHT_SHARING/None/ADP-Release1_CRC_OSDataset_combined_norm_target_domain_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0001/best_trial_1_date_2021-07-21-07-17-15.pth.tar \
 --dataset_name=BACH_transformed \
---output_path=output/BACH_ws_ADP+CRC+OS_nocoloraug_imagenet \
+--output_path=output/BACH_ws_ADP+CRC+OS_nocoloraug_none \
 --aug_smooth=True --eigen_smooth=True
 
 python3 main.py --image_path=$ROOT/examples/BACH/ \
 --model_path=$WEIGHT_SHARING/None/ADP-Release1_CRC_BCSS_combined_norm_target_domain_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0001/best_trial_1_date_2021-07-20-22-00-49.pth.tar \
 --dataset_name=BACH_transformed \
---output_path=output/BACH_ws_ADP+CRC+BCSS_nocoloraug_imagenet \
+--output_path=output/BACH_ws_ADP+CRC+BCSS_nocoloraug_none \
 --aug_smooth=True --eigen_smooth=True
 
 python3 main.py --image_path=$ROOT/examples/BACH/ \
 --model_path=$WEIGHT_SHARING/None/ADP-Release1_CRC_OSDataset_BCSS_combined_norm_target_domain_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0002/best_trial_2_date_2021-07-21-01-06-57.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_ws_ADP+CRC+OS+BCSS_nocoloraug_none \
+--aug_smooth=True --eigen_smooth=True
+
+# ImageNet results
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=/ssd2/HistoKT/results/post_training_without_color_aug_ImageNet_norm_ImageNet/BCSS_transformed_norm_ImageNet_color_aug_None_ImageNet/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.00005/best_trial_2_date_2021-07-18-09-27-33.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_BCSSpost_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=/ssd2/HistoKT/results/post_training_without_color_aug_ImageNet_norm_ImageNet/OSDataset_transformed_norm_ImageNet_color_aug_None_ImageNet/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.00005/best_trial_1_date_2021-07-18-09-46-33.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_OSpost_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=$WEIGHT_SHARING/ImageNet/ADP-Release1_AJ-Lymph_BACH_BCSS_CRC_GlaS_MHIST_OSDataset_PCam_combined_ImageNet_norm_ImageNet_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0005/best_trial_2_date_2021-07-20-18-57-13.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_ws_all_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=$WEIGHT_SHARING/ImageNet/ADP-Release1_CRC_combined_ImageNet_norm_ImageNet_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.001/best_trial_1_date_2021-07-21-04-27-15.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_ws_ADP+CRC_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=$WEIGHT_SHARING/ImageNet/ADP-Release1_BCSS_combined_ImageNet_norm_ImageNet_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0005/best_trial_2_date_2021-07-21-05-13-57.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_ws_ADP+BCSS_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=$WEIGHT_SHARING/ImageNet/ADP-Release1_CRC_OSDataset_combined_ImageNet_norm_ImageNet_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.001/best_trial_0_date_2021-07-20-17-58-11.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_ws_ADP+CRC+OS_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=$WEIGHT_SHARING/ImageNet/ADP-Release1_CRC_BCSS_combined_ImageNet_norm_ImageNet_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0005/best_trial_0_date_2021-07-21-00-05-19.pth.tar \
+--dataset_name=BACH_transformed \
+--output_path=output/BACH_ws_ADP+CRC+BCSS_nocoloraug_imagenet \
+--aug_smooth=True --eigen_smooth=True
+
+python3 main.py --image_path=$ROOT/examples/BACH/ \
+--model_path=$WEIGHT_SHARING/ImageNet/ADP-Release1_CRC_OSDataset_BCSS_combined_ImageNet_norm_ImageNet_color_aug_None/BACH_transformed/AdamP/checkpoint/deep_tuning/lr-0.0005/best_trial_1_date_2021-07-21-00-14-39.pth.tar \
 --dataset_name=BACH_transformed \
 --output_path=output/BACH_ws_ADP+CRC+OS+BCSS_nocoloraug_imagenet \
 --aug_smooth=True --eigen_smooth=True
