00b80259e17f00b5a9c5ab0f096de9ef4c35a448
epicrunze <epicrunze@gmail.com>
2022-01-03 19:36:08 -0500

    combined dataset change

--- src/adas/image_transforms/custom_augmentations.py
+++ src/adas/image_transforms/custom_augmentations.py
@@ -103,7 +103,6 @@ class ColorDistortion:
         color_jitter = transforms.ColorJitter(0.8 * self.distortion, 0.8 * self.distortion,
                                               0.8 * self.distortion, 0.2 * self.distortion)
         rnd_color_jitter = transforms.RandomApply([color_jitter], p=1.0)
-        rnd_gray = transforms.RandomGrayscale(p=0.2)
         color_distort = transforms.Compose([
             rnd_color_jitter,
             # rnd_gray
